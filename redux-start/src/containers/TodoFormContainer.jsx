8d77401aea6e703da8d084530797089c34f05ffe
l0u0h0 <l0u0h0@gmail.com>
2022-05-13 17:46:41 +0900

    react-redux_3

--- redux-start/src/containers/TodoFormContainer.jsx
+++ redux-start/src/containers/TodoFormContainer.jsx
@@ -1,14 +1,27 @@
-import { connect } from "react-redux";
+import { useCallback } from "react";
+import { connect, useDispatch } from "react-redux";
 import TodoForm from "../components/TodoForm";
 import { addTodo } from "../redux/actions";
 
-const TodoFormContainer = connect(
-  (state) => ({}),
-  (dispatch) => ({
-    add: (text) => {
+// const TodoFormContainer = connect(
+//   (state) => ({}),
+//   (dispatch) => ({
+//     add: (text) => {
+//       dispatch(addTodo(text));
+//     },
+//   })
+// )(TodoForm);
+
+// export default TodoFormContainer;
+
+export default function TodoFormContainer() {
+  const dispatch = useDispatch();
+  // [dispatch]에서 변화가 있을 때 함수가 새로 만들어짐
+  const add = useCallback(
+    (text) => {
       dispatch(addTodo(text));
     },
-  })
-)(TodoForm);
-
-export default TodoFormContainer;
+    [dispatch]
+  );
+  return <TodoForm add={add} />;
+}
--- redux-start/src/containers/TodoListContainer.jsx
+++ redux-start/src/containers/TodoListContainer.jsx
@@ -1,19 +1,25 @@
-import { connect } from "react-redux";
+import { connect, useSelector } from "react-redux";
 import TodoList from "../components/TodoList";
 
-const mapStateToProps = (state) => {
-  return {
-    todos: state.todos,
-  };
-};
-const mapDispatchToProps = (dispatch) => {
-  return {};
-};
-// connect 함수를 실행한 결과가 함수이기에 뒤에 ()
-// connect 함수를 실행한 결과가 함수고 그 함수를 실행한 결과가 컨테이너
-const TodoListContainer = connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(TodoList);
+// const mapStateToProps = (state) => {
+//   return {
+//     todos: state.todos,
+//   };
+// };
+// const mapDispatchToProps = (dispatch) => {
+//   return {};
+// };
+// // connect 함수를 실행한 결과가 함수이기에 뒤에 ()
+// // connect 함수를 실행한 결과가 함수고 그 함수를 실행한 결과가 컨테이너
+// const TodoListContainer = connect(
+//   mapStateToProps,
+//   mapDispatchToProps
+// )(TodoList);
+
+function TodoListContainer() {
+  const todos = useSelector((state) => state.todos);
+
+  return <TodoList todos={todos} />;
+}
 
 export default TodoListContainer;
