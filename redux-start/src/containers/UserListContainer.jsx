d48f419ca740acd2606927d0eac4e7f182f49b8a
l0u0h0 <l0u0h0@gmail.com>
2022-05-26 23:54:30 +0900

    redux-thunk

--- redux-start/src/containers/UserListContainer.jsx
+++ redux-start/src/containers/UserListContainer.jsx
@@ -1,26 +1,34 @@
 import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import UserList from "../components/UserList";
-import { getUsersFail, getUsersStart, getUsersSuccess } from "../redux/actions";
-import axios from "axios";
+import {
+  // getUsersFail,
+  // getUsersStart,
+  // getUsersSuccess,
+  getUsersThunk,
+} from "../redux/actions";
+// import axios from "axios";
 
 export default function UserListContainer() {
   const users = useSelector((state) => state.users.data);
   const dispatch = useDispatch();
 
   // 비동기 작업
-  const getUsers = useCallback(
-    async function getUsers() {
-      try {
-        dispatch(getUsersStart());
-        const res = await axios.get("https://api.github.com/users");
-        dispatch(getUsersSuccess(res.data));
-      } catch (error) {
-        dispatch(getUsersFail(error));
-      }
-    },
-    [dispatch]
-  );
+  // const getUsers = useCallback(
+  //   async function getUsers() {
+  //     try {
+  //       dispatch(getUsersStart());
+  //       const res = await axios.get("https://api.github.com/users");
+  //       dispatch(getUsersSuccess(res.data));
+  //     } catch (error) {
+  //       dispatch(getUsersFail(error));
+  //     }
+  //   },
+  //   [dispatch]
+  // );
 
+  const getUsers = useCallback(() => {
+    dispatch(getUsersThunk);
+  }, [dispatch]);
   return <UserList users={users} getUsers={getUsers} />;
 }
